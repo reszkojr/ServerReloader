1eb5c59179e354cac12086e7cf03d922c0cd8b2d
reszkojr <s.reszkojr@gmail.com>
2022-04-29 14:09:22 -0300

    Added a message when the server is going to reload

--- src/me/santres/serverreloader/FileEventNotifier.java
+++ src/me/santres/serverreloader/FileEventNotifier.java
@@ -1,6 +1,11 @@
 package me.santres.serverreloader;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import java.nio.file.StandardWatchEventKinds;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 public class FileEventNotifier implements Observer {
 
@@ -13,6 +18,19 @@ public class FileEventNotifier implements Observer {
     @Override
     public void update(UpdatedFile file) {
         ServerReloader.sendConsoleMessage("File: " + file.getName() + ", Event: " + file.getEventKind());
+        String verb;
+        if (ENTRY_CREATE == file.getEventKind()) {
+            verb = "added";
+        } else if (ENTRY_MODIFY == file.getEventKind()) {
+            verb = "modified";
+        } else if (ENTRY_DELETE == file.getEventKind()) {
+            verb = "deleted";
+        } else {
+            verb = "nhamnhamnhamnham";
+        }
+
+        Bukkit.broadcastMessage(ChatColor.GREEN + file.getName() + " was " + verb + "!");
+        Bukkit.broadcastMessage(ChatColor.GREEN + "Reloading server...");
         Bukkit.getServer().reload();
     }
 }
